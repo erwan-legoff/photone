9fce2fbaa1b8ae27c9553def280ecc227f56122d
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2024-05-30 22:08:01 +0200

    adding the formatting behavior

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1>Salut ! Tu es sur la première page de erwan</h1>
     <p>Ici tu vas pouvoir uploader une image</p>
     <div>
-      <input type="file" id="file" name="file" accept="image/png, image/jpeg" />
+      <input id="file" type="file" name="file" accept="image/png, image/jpeg" >
     </div>
   </div>
 </template>
