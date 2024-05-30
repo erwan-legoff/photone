1928ea39df479c1af50c9935c55ac3968bc5a08a
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2024-05-30 21:54:57 +0200

    initialized working router

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Salut ! Tu es sur la première page de erwan</h1>
+    <p>Ici tu vas pouvoir uploader une image</p>
+    <div>
+      <input type="file" id="file" name="file" accept="image/png, image/jpeg" />
+    </div>
+  </div>
+</template>
