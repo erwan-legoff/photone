b6d3f24c9de188fdb4c43e277b28b809d56aeb9c
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2024-11-08 07:41:06 +0100

    Photos now take all the screen space and the button is next to the file field

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,35 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications
 //https://image.nuxt.com/usage/nuxt-img
 <template>
-  <v-layout>
+  <v-container>
     <div>
       <h1>Salut ! Tu es sur la première page de erwan</h1>
       <p>Ici tu vas pouvoir uploader une image</p>
       <div>
-        <v-file-input v-model="mediumToUpload" label="File input" accept="image/*" />
-        <v-btn @click="upload">
-          upload
-
-        </v-btn>
         <v-row>
-          <v-col v-for="medium in media" :key="medium.name" cols="12" sm="6" md="4">
-            <v-card flat tile>
-              <v-img :src="createURL(medium)" aspect-ratio="1" class="white--text align-end" height="200">
-                <!-- Vous pouvez ajouter du contenu overlay ici si nécessaire -->
-              </v-img>
-            </v-card>
-          </v-col>
+          <v-file-input v-model="mediumToUpload" label="File input" accept="image/*" />
+          <v-btn @click="upload">
+            upload
+
+          </v-btn>
         </v-row>
       </div>
-
-
-      <div />
+      <v-row>
+        <v-col v-for="medium in media" :key="medium.name" cols="12" sm="6" md="4">
+          <v-card flat tile>
+            <v-img :src="createURL(medium)" aspect-ratio="1" class="white--text align-end" height="200">
+              <!-- Vous pouvez ajouter du contenu overlay ici si nécessaire -->
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
-  </v-layout>
+
+
+
+
+
+  </v-container>
 </template>
 
 <script setup lang="ts">
