7889090a20eaaed8c051267eec32c6fd5325d5f5
erwil <rwan.legoff@gmail.com>
2025-01-12 17:53:06 +0100

    Now can upload multiple photos

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,15 +8,23 @@
         <p>Choose a file to upload to your private space</p>
         <div>
           <v-row>
-            <v-file-input v-model="mediumToUpload" label="File input" accept="image/*" />
-            <v-btn @click="upload">
-              upload
-
-            </v-btn>
+            <v-file-input
+              v-model="mediumToUpload"
+              label="File input"
+              accept="image/*"
+              :multiple="true"
+            />
+            <v-btn @click="uploadMany"> upload </v-btn>
           </v-row>
         </div>
         <v-row>
-          <v-col v-for="medium in media" :key="medium.name" cols="12" sm="6" md="4">
+          <v-col
+            v-for="medium in media"
+            :key="medium.name"
+            cols="12"
+            sm="6"
+            md="4"
+          >
             <v-card tile>
               <v-img :src="createURL(medium)">
                 <!-- Vous pouvez ajouter du contenu overlay ici si nécessaire -->
@@ -33,25 +41,37 @@
 import { useMediumStore } from '~/stores/uploadStore'
 const mediumStore = useMediumStore()
 
-
 const mediumToUpload: Ref<File | File[] | null | undefined> = ref(null)
 const media = computed(() => mediumStore.media)
 
-const upload = async () => {
+const uploadOne = async () => {
   console.log("let's upload")
-  if (!mediumToUpload.value) return console.log("photos is undefined or null")
-  if ((mediumToUpload.value as Array<File>).length) return console.log("this is an array")
-  console.log("everything seems ok")
+  if (!mediumToUpload.value) return console.log('photos is undefined or null')
+  if ((mediumToUpload.value as Array<File>).length)
+    return console.log('this is an array')
+  console.log('everything seems ok')
   const photoFile = mediumToUpload.value as File
 
   await mediumStore.uploadMedium(photoFile)
   downloadAll()
 }
 
+const uploadMany = async () => {
+  const filesToUpload: Array<File> = []
+  if (!mediumToUpload.value) return console.log('photos is undefined or null')
+  if (!Array.isArray(mediumToUpload.value)) {
+    console.log('this is not an array')
+    return await mediumStore.uploadMedia([mediumToUpload.value as File])
+  }
+  if ((mediumToUpload.value as File[]).length) {
+    console.log('this is an array')
+    return await mediumStore.uploadMedia(mediumToUpload.value as File[])
+  }
+}
+
 const downloadAll = async () => {
   console.log('lets download')
   mediumStore.fetchMedia()
-
 }
 
 const createURL = (file: File) => {
@@ -62,10 +82,5 @@ onMounted(() => {
   downloadAll()
 })
 
-
 //
-
-
-
-
 </script>
--- app/stores/uploadStore.ts
+++ app/stores/uploadStore.ts
@@ -1,24 +1,26 @@
 //https://pinia.vuejs.org/core-concepts/state.html
 import { defineStore } from 'pinia'
-import type { GetMediumDto } from './types/GetMediumDto';
-const SERVICE_ROOT =  process.env.NUXT_MEDIUM_SERVICE_ROOT || 'http://localhost:8080'
+import type { GetMediumDto } from './types/GetMediumDto'
+const SERVICE_ROOT =
+  process.env.NUXT_MEDIUM_SERVICE_ROOT || 'http://localhost:8080'
 //import type { PhotoDto } from './types/PhotoDto'
 //https://nuxt.com/docs/getting-started/state-management
 
+const UPLOAD_MEDIUM_URL = SERVICE_ROOT + '/medium'
+const UPLOAD_MEDIA_URL = SERVICE_ROOT + '/media'
 export interface MediumState {
   media: File[]
   mediaAreLoading: boolean
   errorMessage: string | null
 }
 
-export const useMediumStore = defineStore('medium-store',{
-  state: ():MediumState => {
+export const useMediumStore = defineStore('medium-store', {
+  state: (): MediumState => {
     return {
       media: [] as File[],
       mediaAreLoading: false as boolean,
-      errorMessage: null
-      
-  }
+      errorMessage: null,
+    }
   },
   actions: {
     /**
@@ -27,34 +29,50 @@ export const useMediumStore = defineStore('medium-store',{
      */
     async uploadMedium(medium: File): Promise<void> {
       try {
- 
-      const UPLOAD_PHOTO_URL = SERVICE_ROOT + '/media'
-      const formData = new FormData();
-      formData.append("medium", medium);
-      console.log('formData', JSON.stringify(formData))
-      
-      const response = await $fetch(UPLOAD_PHOTO_URL, {
-          method: "POST",
+        const formData = new FormData()
+        formData.append('medium', medium)
+        console.log('formData', JSON.stringify(formData))
+
+        const response = await $fetch(UPLOAD_MEDIUM_URL, {
+          method: 'POST',
           body: formData,
-        });
-      
-      
+        })
+
         console.log(JSON.stringify(response))
       } catch (error) {
         this.handleError(error, 'uploadMedium')
         throw error
-        
+      }
+    },
+    async uploadMedia(media: File[]): Promise<void> {
+      try {
+        const formData = new FormData()
+
+        media.forEach((file) => {
+          formData.append("media", file)
+        })
+
+
+        const response = await $fetch(UPLOAD_MEDIA_URL, {
+          method: 'POST',
+
+          body: formData,
+        })
+
+        console.log(JSON.stringify(response))
+      } catch (error) {
+        this.handleError(error, 'uploadMedia')
+        throw error
       }
     },
 
-    async fetchMedium(getMediumDto: GetMediumDto): Promise<File>{
+    async fetchMedium(getMediumDto: GetMediumDto): Promise<File> {
       const GET_MEDIUM_URL = SERVICE_ROOT + '/medium'
       try {
         const response = await $fetch<File>(GET_MEDIUM_URL, {
-          method: "GET",
-          params:getMediumDto
-     
-        });
+          method: 'GET',
+          params: getMediumDto,
+        })
         return response
       } catch (error: unknown) {
         this.handleError(error, 'getMedium')
@@ -62,12 +80,12 @@ export const useMediumStore = defineStore('medium-store',{
       }
     },
 
-    async fetchMediumUrls(): Promise<Array<string>>{
+    async fetchMediumUrls(): Promise<Array<string>> {
       const GET_MEDIUM_URL = SERVICE_ROOT + '/media'
       try {
         const response = await $fetch<Array<string>>(GET_MEDIUM_URL, {
-          method: "GET",     
-        });
+          method: 'GET',
+        })
         return response
       } catch (error: unknown) {
         this.handleError(error, 'getMediumUrls')
@@ -75,16 +93,14 @@ export const useMediumStore = defineStore('medium-store',{
       }
     },
 
-    async fetchMedia(): Promise<void>{
-
+    async fetchMedia(): Promise<void> {
       try {
-        const mediumUrls = await this.fetchMediumUrls();
-        const fetchPromises = mediumUrls.map(url =>
+        const mediumUrls = await this.fetchMediumUrls()
+        const fetchPromises = mediumUrls.map((url) =>
           $fetch<File>(url, { method: 'GET' })
         )
         const files = await Promise.all(fetchPromises)
         this.media = files
-
       } catch (error: unknown) {
         this.handleError(error, 'getMedia')
         throw error
@@ -96,7 +112,7 @@ export const useMediumStore = defineStore('medium-store',{
     isMediaLoading() {
       return this.isMediaLoading
     },
-     /**
+    /**
      * Gère les erreurs en les enregistrant dans l'état et en les loggant.
      * @param error - L'erreur capturée.
      * @param action - L'action où l'erreur s'est produite.
@@ -110,7 +126,5 @@ export const useMediumStore = defineStore('medium-store',{
         this.errorMessage = `Erreur inconnue dans ${action}`
       }
     },
-
-
   },
 })
