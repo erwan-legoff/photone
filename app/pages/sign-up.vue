034ce375f964f8b47ac787d5f60bcc86d76a2a2f
erwil <rw4nit0@gmail.com>
2025-04-12 22:32:10 +0200

    WIP : a complete form

--- app/pages/sign-up.vue
+++ app/pages/sign-up.vue
@@ -3,25 +3,57 @@
       <v-sheet class="pa-6 rounded" max-width="400" elevation="6">
         <v-form>
           <v-row dense>
-            <v-col cols="12">
+            <v-col cols="6">
               <v-text-field
                 v-model="firstName"
                 label="First name"
+                variant="outlined"
+                color="primary"
+
+              />
+            </v-col>
+  
+            <v-col cols="6">
+              <v-text-field
+                v-model="lastName"
+                label="Last name"
+                variant="outlined"
+                color="primary"
+
+              />
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field
+                v-model="pseudo"
+                label="Pseudo"
+                variant="outlined"
+                color="primary"
+
+              />
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field
+                v-model="email"
+                label="Email"
                 variant="outlined"
                 color="primary"
                 
                 clearable
               />
             </v-col>
-  
+
             <v-col cols="12">
               <v-text-field
-                v-model="lastName"
-                label="Last name"
+                v-model="password"
+                :type="showPassword ? 'text' : 'password'"
+                label="Password"
                 variant="outlined"
                 color="primary"
-                
-                clearable
+                :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                @click:append-inner="showPassword = !showPassword"
+
               />
             </v-col>
   
@@ -32,7 +64,6 @@
                 color="primary"
                 class="mt-4"
                 size="large"
-                
               >
                 Submit
               </v-btn>
@@ -47,6 +78,8 @@ const firstName = ref('')
 const lastName = ref('')
 const pseudo = ref('')
 const email = ref('')
+const password = ref('')
+const showPassword = ref(false)
 
 
 </script>
