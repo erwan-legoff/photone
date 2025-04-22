477060580f6fcff3f906e4b379fa08fbdbe4f511
erwil <rw4nit0@gmail.com>
2025-04-22 10:53:21 +0200

    A 401 response will trigger a login redirection

--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -1,10 +1,18 @@
-export default defineNuxtPlugin(async () => {
+export default defineNuxtPlugin(() => {
   const service_root =
     process.env.NUXT_MEDIUM_SERVICE_ROOT || "http://localhost:8080";
 
   const apiFetch = $fetch.create({
     baseURL: service_root,
     credentials: "include",
+    onResponseError({ response }) {
+      if (response.status === 401) {
+        const userStore = useUserStore();
+        userStore.logout();
+
+        navigateTo("/login", { replace: true });
+      }
+    },
   });
 
   return {
