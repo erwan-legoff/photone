9dcd1b2c205879963adc15bea464801f0537345f
erwil <rwan.legoff@gmail.com>
2025-05-06 11:51:26 +0200

    deriveKeyFromPassword

--- app/tools/security/encryption/deriveKeyFromPassword.ts
+++ app/tools/security/encryption/deriveKeyFromPassword.ts
@@ -11,13 +11,29 @@ export async function deriveKeyFromPassword(
   const keyFormat = 'raw'
   const algorithmIdentifier: AlgorithmIdentifier = { name: 'PBKDF2' }
   const isExtractable = false
-  const keyUsage: KeyUsage[] = ['deriveKey']
 
   const keyMaterial = await crypto.subtle.importKey(
     keyFormat,
     encoder.encode(password),
     algorithmIdentifier,
     isExtractable,
-    keyUsage
+    ['deriveKey']
+  )
+  const derivationAlgorithm: Pbkdf2Params = {
+    name: 'PBKBF2',
+    salt: salt,
+    iterations: 250000,
+    hash: 'SHA-256',
+  }
+  const encryptionAlgorithm: AesDerivedKeyParams = {
+    name: 'AES-GCM',
+    length: 256,
+  }
+  return crypto.subtle.deriveKey(
+    derivationAlgorithm,
+    keyMaterial,
+    encryptionAlgorithm,
+    true,
+    ['encrypt', 'decrypt']
   )
 }
