7281220406a0f743e949f1c719e5142b63887503
Erwan Le Goff <rwan.legoff@gmail.com>
2025-06-29 18:29:13 +0200

    reorganizing translation

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,7 +13,8 @@
           </div>
 
           <v-alert type="info" variant="tonal" border="start" density="compact" class="mb-8 rounded" icon="mdi-wrench">
-            <span class="font-weight-medium">{{ $t('home_page.note') }}</span> {{ $t('home_page.noteContent') }}
+            <span class="font-weight-medium">{{ $t('home_page.note') }}</span>
+            {{ $t('home_page.noteContent') }}
           </v-alert>
 
           <section class="text-center mb-10">
@@ -22,9 +23,10 @@
             </h2>
             <div class="d-flex justify-center flex-wrap ga-4">
               <v-btn to="/photos" color="primary" size="large" rounded="lg" class="text-none">
-                {{ $t('home_page.goToPhotos') }}
+                {{ $t('home_page.openApplication') }}
               </v-btn>
-              <v-btn href="mailto:rw4nit0@gmail.com" color="secondary" size="large" rounded="lg" class="text-none" prepend-icon="mdi-email-fast">
+              <v-btn href="mailto:rw4nit0@gmail.com" color="secondary" size="large" rounded="lg" class="text-none"
+                prepend-icon="mdi-email-fast">
                 {{ $t('home_page.sendMail') }}
               </v-btn>
             </div>
@@ -46,58 +48,93 @@
           <section class="mb-10">
             <h2 class="text-h5 font-weight-semibold mb-4 border-b pb-2">
               <v-icon start icon="mdi-code-tags" />
-              {{ $t('home_page.techTitle') }}
+              {{ $t('home_page.tech.title') }}
             </h2>
             <div class="d-flex flex-wrap ga-2">
-              <v-chip color="primary" variant="flat" prepend-icon="mdi-language-java">{{ $t('home_page.techSpring')
-                }}</v-chip>
-              <v-chip color="primary" variant="flat" prepend-icon="mdi-nuxt">{{ $t('home_page.techNuxt') }}</v-chip>
-              <v-chip color="primary" variant="flat" prepend-icon="mdi-vuetify">{{ $t('home_page.techVuetify')
-                }}</v-chip>
-              <v-chip color="primary" variant="flat" prepend-icon="mdi-tailwind">{{ $t('home_page.techTailwind')
-                }}</v-chip>
-              <v-chip color="primary" variant="flat" prepend-icon="mdi-security">{{ $t('home_page.techJWT') }}</v-chip>
+              <v-chip color="primary" variant="flat" prepend-icon="mdi-language-java">
+                {{ $t('home_page.tech.spring') }}
+              </v-chip>
+              <v-chip color="primary" variant="flat" prepend-icon="mdi-nuxt">
+                {{ $t('home_page.tech.nuxt') }}
+              </v-chip>
+              <v-chip color="primary" variant="flat" prepend-icon="mdi-vuetify">
+                {{ $t('home_page.tech.vuetify') }}
+              </v-chip>
+              <v-chip color="primary" variant="flat" prepend-icon="mdi-tailwind">
+                {{ $t('home_page.tech.tailwind') }}
+              </v-chip>
+              <v-chip color="primary" variant="flat" prepend-icon="mdi-security">
+                {{ $t('home_page.tech.jwt') }}
+              </v-chip>
             </div>
           </section>
 
           <section class="mb-10">
             <h2 class="text-h5 font-weight-semibold mb-4 border-b pb-2">
               <v-icon start icon="mdi-cogs" />
-              {{ $t('home_page.featuresTitle') }}
+              {{ $t('home_page.features.title') }}
             </h2>
             <v-row>
               <v-col cols="12" md="6">
                 <h3 class="text-h6 font-weight-medium mb-3">
                   <v-icon start icon="mdi-check-circle" color="success" />
-                  {{ $t('home_page.featuresDone') }}
+                  {{ $t('home_page.features.done') }}
                 </h3>
                 <v-list density="compact" class="bg-transparent">
-                  <v-list-item prepend-icon="mdi-upload">{{ $t('home_page.featureUpload') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-database-lock">{{ $t('home_page.featureStorage') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-image-multiple">{{ $t('home_page.featureDisplay') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-api">{{ $t('home_page.featureApi') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-account-plus">{{ $t('home_page.featureSignup') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-security">{{ $t('home_page.featureAuth') }}</v-list-item>
+                  <v-list-item prepend-icon="mdi-upload">
+                    {{ $t('home_page.features.upload') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-database-lock">
+                    {{ $t('home_page.features.storage') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-image-multiple">
+                    {{ $t('home_page.features.display') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-api">
+                    {{ $t('home_page.features.api') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-account-plus">
+                    {{ $t('home_page.features.signup') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-security">
+                    {{ $t('home_page.features.auth') }}
+                  </v-list-item>
                 </v-list>
               </v-col>
 
               <v-col cols="12" md="6">
                 <h3 class="text-h6 font-weight-medium mb-3">
                   <v-icon start icon="mdi-progress-wrench" color="warning" />
-                  {{ $t('home_page.featuresPlanned') }}
+                  {{ $t('home_page.features.planned') }}
                 </h3>
                 <v-list density="compact" class="bg-transparent">
-                  <v-list-item prepend-icon="mdi-key-variant">{{ $t('home_page.featureReset') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-email-check">{{ $t('home_page.featureMail') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-shield-lock">{{ $t('home_page.featureEncrypt') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-folder-multiple-image">{{ $t('home_page.featureAlbums')
-                    }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-account-group">{{ $t('home_page.featureGroups') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-share-variant">{{ $t('home_page.featureShare') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-tag-multiple">{{ $t('home_page.featureTags') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-sync">{{ $t('home_page.featureSync') }}</v-list-item>
-                  <v-list-item prepend-icon="mdi-desktop-mac-dashboard">{{ $t('home_page.featureDesktop')
-                    }}</v-list-item>
+                  <v-list-item prepend-icon="mdi-key-variant">
+                    {{ $t('home_page.features.reset') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-email-check">
+                    {{ $t('home_page.features.mail') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-shield-lock">
+                    {{ $t('home_page.features.encrypt') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-folder-multiple-image">
+                    {{ $t('home_page.features.albums') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-account-group">
+                    {{ $t('home_page.features.groups') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-share-variant">
+                    {{ $t('home_page.features.share') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-tag-multiple">
+                    {{ $t('home_page.features.tags') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-sync">
+                    {{ $t('home_page.features.sync') }}
+                  </v-list-item>
+                  <v-list-item prepend-icon="mdi-desktop-mac-dashboard">
+                    {{ $t('home_page.features.desktop') }}
+                  </v-list-item>
                 </v-list>
               </v-col>
             </v-row>
