27c1c431b0cf7957534f2eb8214f4f8e24763432
Erwan Le Goff <rwan.legoff@gmail.com>
2025-08-17 13:20:13 +0200

    Now we validate the input before sending it to the server.

--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -20,6 +20,8 @@
             <v-text-field v-model="email" :label="$t('global.email')" variant="outlined" color="primary" clearable />
           </v-col>
 
+
+          Le mot de passe doit contenir au moins 6 caractères
           <v-col cols="12">
             <v-text-field v-model="password" :type="showPassword ? 'text' : 'password'" :label="$t('signup.password')"
               variant="outlined" color="primary" :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
@@ -27,7 +29,12 @@
           </v-col>
 
           <v-col cols="12">
-            <v-btn block color="primary" class="mt-4" size="large" type="submit">
+            <v-text-field v-model="copiedPassword" type="password" :label="$t('signup.rewritePassword')"
+              variant="outlined" color="primary"></v-text-field>
+          </v-col>
+
+          <v-col cols="12">
+            <v-btn block color="primary" class="mt-4" size="large" type="submit" :disabled="!canSubmit">
               {{ $t('signup.submit') }}
             </v-btn>
           </v-col>
@@ -56,20 +63,39 @@ const lastName = ref('')
 const pseudo = ref('')
 const email = ref('')
 const password = ref('')
+const copiedPassword = ref('')
+const isFirstNameValid = computed(() => {
+  const trimed = firstName.value.trim()
+  return trimed.length > 1 && trimed.length < 60
+})
+const isLastNameValid = computed(() => {
+  const trimed = lastName.value.trim()
+  return trimed.length > 1 && trimed.length < 100
+})
+const isPseudoValid = computed(() => {
+  const trimed = pseudo.value.trim()
+  return trimed.length >= 3 && trimed.length < 30
+})
+const isPasswordValid = computed(() => { return password.value.length >= 6 })
+const isCopiedPasswordValid = computed(() => { return password.value === copiedPassword.value })
+const isEmailValid = computed(() => { return email.value.trim().length > 5 && email.value.length < 60 && email.value.includes("@") && email.value.includes(".") })
+const canSubmit = computed(() => { return isFirstNameValid.value && isLastNameValid.value && isPseudoValid.value && isEmailValid.value && isPasswordValid.value && isCopiedPasswordValid.value })
 const showPassword = ref(false)
+// We don't want them to copy a visible password
+watch(copiedPassword, () => { if (copiedPassword.value.length > 0) { showPassword.value = false } })
+// If they check, they're not sure if it's the good password
+watch(showPassword, () => { if (showPassword.value) { copiedPassword.value = "" } })
 
 const createUserDto = computed<CreateUserRequestDto>(() => ({
-  pseudo: pseudo.value,
-  email: email.value,
+  pseudo: pseudo.value.trim(),
+  email: email.value.trim().toLowerCase(),
   rawPassword: password.value,
-  name: firstName.value,
-  lastName: lastName.value
+  name: firstName.value.trim(),
+  lastName: lastName.value.trim()
 }))
 const userStore = useUserStore()
 const submit = async () => {
-  console.log('createUserDto:', createUserDto.value)
   const isUserCreated: boolean = await userStore.createUser(createUserDto.value)
-  console.log('isUserCreated:', isUserCreated)
   const localePath = useLocalePath();
   if (isUserCreated) navigateTo(localePath({ name: 'mail-verification' }))
 }
