47915bf6462581d63c54a7b8f3f24ac32d327965
Erwan Le Goff <rwan.legoff@gmail.com>
2025-08-18 17:18:55 +0200

    adding popup for creating an album

--- app/pages/albums.vue
+++ app/pages/albums.vue
@@ -1,30 +1,60 @@
 <template>
   <div class="full-page-background">
     <v-container>
-      <form @submit.prevent="submit">
-        <v-text-field v-model="title" label="Titre">
+      <v-row>
+        <v-col cols="12" class="d-flex justify-end">
+          <v-btn color="primary" @click="dialog = true">{{ t('albums.createButton') }}</v-btn>
+        </v-col>
+      </v-row>
 
-        </v-text-field>
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card>
+          <v-card-title>{{ t('albums.createDialogTitle') }}</v-card-title>
 
-        <v-text-field v-model="description" label="Description">
+          <v-card-text>
+            <v-form @submit.prevent="submit">
+              <v-text-field v-model="title" :label="t('albums.titleLabel')" required></v-text-field>
+              <v-text-field v-model="description" :label="t('albums.descriptionLabel')"></v-text-field>
+            </v-form>
+          </v-card-text>
 
-        </v-text-field>
-        <v-btn class="mt-2" text="Créer Album" type="submit" :disabled="!canSubmit" block></v-btn>
-
-
-      </form>
-
-
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="onCancel">{{ t('common.cancel') }}</v-btn>
+            <v-btn :disabled="!canSubmit" color="primary" @click="submit">{{ t('albums.createButton') }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useAlbumStore } from '~/stores/albumStore';
+import { ref, computed } from 'vue'
+import { useAlbumStore } from '~/stores/albumStore'
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 const albumStore = useAlbumStore()
-const title = ref("")
-const description = ref("")
-const canSubmit = computed(() => title.value.length > 0)
-const submit = () => { albumStore.createAlbum({ title: title.value, description: description.value }) }
+const dialog = ref(false)
+const title = ref('')
+const description = ref('')
+const canSubmit = computed(() => title.value.trim().length > 0)
+
+const resetForm = () => {
+  title.value = ''
+  description.value = ''
+}
+
+const onCancel = () => {
+  resetForm()
+  dialog.value = false
+}
+
+const submit = async () => {
+  if (!canSubmit.value) return
+  await albumStore.createAlbum({ title: title.value, description: description.value })
+  resetForm()
+  dialog.value = false
+}
 </script>
