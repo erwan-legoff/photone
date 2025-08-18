38856c1f5ffe584c74e89c8e1cf691b35635aa77
Erwan Le Goff <rwan.legoff@gmail.com>
2025-08-18 15:55:34 +0200

    quick format

--- app/components/PhotoGrid.vue
+++ app/components/PhotoGrid.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-row class="photo-grid" dense>
-    <v-col v-for="(medium, i) in media" :key="medium.id" cols="12" sm="6" md="4" xl="2">
-      <v-card class="overflow-hidden rounded-lg" elevation="2">
-        <v-img :src="createURL(medium.file)" height="200" cover @click="() => emitOpen(i)">
-          <template #placeholder>
-            <div class="d-flex align-center justify-center fill-height">
-              <v-progress-circular color="grey-lighten-4" indeterminate />
-            </div>
-          </template>
-        </v-img>
-      </v-card>
-    </v-col>
-  </v-row>
+    <v-row class="photo-grid" dense>
+        <v-col v-for="(medium, i) in media" :key="medium.id" cols="12" sm="6" md="4" xl="2">
+            <v-card class="overflow-hidden rounded-lg" elevation="2">
+                <v-img :src="createURL(medium.file)" height="200" cover @click="() => emitOpen(i)">
+                    <template #placeholder>
+                        <div class="d-flex align-center justify-center fill-height">
+                            <v-progress-circular color="grey-lighten-4" indeterminate />
+                        </div>
+                    </template>
+                </v-img>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script setup lang="ts">
@@ -19,23 +19,23 @@ import { defineProps, defineEmits } from 'vue'
 
 const props = defineProps<{ media: Array<any> }>()
 const emits = defineEmits<{
-  (e: 'open', index: number): void
+    (e: 'open', index: number): void
 }>()
 
 const emitOpen = (i: number) => emits('open', i)
 
 const createURL = (file: File) => {
-  try {
-    return URL.createObjectURL(file)
-  } catch (e) {
-    return ''
-  }
+    try {
+        return URL.createObjectURL(file)
+    } catch (e) {
+        return ''
+    }
 }
 </script>
 
 <style scoped>
 .photo-grid .v-img {
-  object-fit: cover;
-  height: 200px;
+    object-fit: cover;
+    height: 200px;
 }
 </style>
